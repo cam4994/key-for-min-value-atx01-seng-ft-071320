40bfcdd7e7fbad1fb4d8d26f6f92ba52176e9022
Colin Mosley <colin.a.mosley@gmail.com>
2020-06-28 18:20:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   min_number = ""
-  min_key = symbol.new
+  min_key = ""
   name_hash.each do |item, value|
     if value < min_number.to_i 
       min_number = value 
@@ -12,3 +12,6 @@ def key_for_min_value(name_hash)
   end 
   min_key
 end
+
+x = :colin 
+print x.class 
