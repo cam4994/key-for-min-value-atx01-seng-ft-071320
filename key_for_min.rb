c01f7778c5074a5763d6a4d8ab88e35a1f8524c3
Colin Mosley <colin.a.mosley@gmail.com>
2020-06-28 18:18:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_number = ""
+  min_key = ""
+  name_hash.each do |item, value|
+    if value < min_number.to_i 
+      min_number = value 
+      min_key = item 
+    end 
+  end 
+  min_key
 end
