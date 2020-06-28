582dfc9a2bfd6c37c0b61e0d1d57154fae9935ce
Colin Mosley <colin.a.mosley@gmail.com>
2020-06-28 18:22:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_number = ""
   min_key = ""
+  min_number = nil 
   name_hash.each do |item, value|
-    if value < min_number.to_i 
+    if min_number == nil 
+      min_number = value 
+    end 
+    if value < min_number
       min_number = value 
       min_key = item 
     end 
   end 
   min_key
 end
-
-x = :colin 
-print x.class 
